01fd98b3e8c535db607e913cd7b76435d0537b3d
soapcandy <octi97@naver.com>
2023-07-24 09:39:00 +0900

    .

---
+++ boot/bg1/src/test/java/com/burgerplace/bg1/Bg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burgerplace.bg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
