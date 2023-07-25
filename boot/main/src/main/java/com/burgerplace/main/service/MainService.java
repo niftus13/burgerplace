6065c2dff3c4d87cf924e25085adea3f03033bea
soapcandy <octi97@naver.com>
2023-07-25 17:40:41 +0900

    작업중

---
+++ boot/main/src/main/java/com/burgerplace/main/service/MainService.java
@@ -0,0 +1,17 @@
+package com.burgerplace.main.service;
+
+import com.burgerplace.main.dto.MainDTO;
+import com.burgerplace.main.dto.MainListRcntDTO;
+import com.burgerplace.main.dto.PageRequestDTO;
+import com.burgerplace.main.dto.PageResponseDTO;
+
+import jakarta.transaction.Transactional;
+
+@Transactional
+public interface MainService {
+    
+    PageResponseDTO<MainListRcntDTO> listRcnt(PageRequestDTO pageRequestDTO);
+
+    MainDTO getOne(Long bno);
+
+}
---
+++ boot/main/src/main/java/com/burgerplace/main/service/MainServiceImpl.java
@@ -0,0 +1,46 @@
+package com.burgerplace.main.service;
+
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.burgerplace.main.dto.MainDTO;
+import com.burgerplace.main.dto.MainListRcntDTO;
+import com.burgerplace.main.dto.PageRequestDTO;
+import com.burgerplace.main.dto.PageResponseDTO;
+import com.burgerplace.main.entity.Main;
+import com.burgerplace.main.repository.MainRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class MainServiceImpl implements MainService {
+    
+    private final MainRepository mainRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public PageResponseDTO<MainListRcntDTO> listRcnt(PageRequestDTO pageRequestDTO) {
+       
+        log.info("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        log.info(pageRequestDTO);
+
+        return mainRepository.searchDTORcnt(pageRequestDTO);
+    }
+
+    @Override
+    public MainDTO getOne(Long bno) {
+       
+        Optional<Main> result = mainRepository.findById(bno);
+        
+        Main main = result.orElseThrow();
+
+        MainDTO dto = modelMapper.map(main, MainDTO.class);
+        
+        return dto;
+    }
+}
