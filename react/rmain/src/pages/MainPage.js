8a5d0ed2e17109972cab35ae699235c672e27975
soapcandy <octi97@naver.com>
2023-07-26 16:39:26 +0900

    게시판 삽입

--- react/rmain/src/pages/MainPage.js
+++ react/rmain/src/pages/MainPage.js
@@ -2,6 +2,9 @@ import { useNavigate } from "react-router-dom";
 import BasicLayout from "../layouts/BasicLayout";
 import { useState } from "react";
 import MainSearchComponent from "../components/main/MainSearchComponent";
+import ListSearchComponent from "../components/board/ListSearchComponent";
+import useQueryObj from "../hooks/useQueryObj";
+import MainListComponent from "../components/main/MainListComponent";
 
 
 
@@ -14,20 +17,44 @@ const MainPage = () => {
     const queryString = new URLSearchParams(searchData).toString();
     navigate(`/board/list?${queryString}`);
   };
-  
 
+  const { queryObj, setSearch, moveRead, moveList } = useQueryObj();
 
-  return ( 
+  const moveSearch2 = (type, keyword) => {
+    queryObj.page = 1;
+    queryObj.type = type;
+    queryObj.keyword = keyword;
+    setSearch({ ...queryObj });
+  };
+  const queryObjWithSize = { ...queryObj, size: 5 };
+
+  return (
     <BasicLayout>
       <h2>Main Page</h2>
-      <h1>TOP3</h1>
+      <h1>============TOP3============</h1>
+      <h1>H-------------------------------------------H</h1>
+      <h1>H-------------------------------------------H</h1>
+      <h1>H-------------------------------------------H</h1>
+      <h1>H-------------------------------------------H</h1>
+      <h1>============TOP3============</h1>
       <MainSearchComponent
         moveSearch={moveSearch}
         queryObj={searchData}
         setSearch={setSearchData}
       />
+      <ListSearchComponent moveSearch={moveSearch} queryObj={queryObj} setSearch={setSearch} />
+      <h1>============작은지도============</h1>
+      <h1>H-----------------------------------------------H</h1>
+      <h1>H-----------------------------------------------H</h1>
+      <h1>H-----------------------------------------------H</h1>
+      <h1>H-----------------------------------------------H</h1>
+      <h1>============작은지도============</h1>
+      <h1>행사상품</h1>
+      <MainListComponent queryObj={queryObjWithSize} movePage={moveList} moveRead={moveRead} />
+      <h1>거래게시판</h1>
+      <h1>자유게시판</h1>
     </BasicLayout>
-   );
+  );
 }
- 
+
 export default MainPage;
