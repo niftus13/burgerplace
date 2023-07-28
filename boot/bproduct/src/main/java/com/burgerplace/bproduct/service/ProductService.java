00e993b8736fa5f78e08f236812f59ce45904558
soapcandy <octi97@naver.com>
2023-07-28 15:28:47 +0900

    .

--- boot/bproduct/src/main/java/com/burgerplace/bproduct/service/ProductService.java
+++ boot/bproduct/src/main/java/com/burgerplace/bproduct/service/ProductService.java
@@ -4,10 +4,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.burgerplace.bproduct.dto.PageRequestDTO;
 import com.burgerplace.bproduct.dto.PageResponseDTO;
+import com.burgerplace.bproduct.dto.ProductDTO;
 import com.burgerplace.bproduct.dto.ProductListDTO;
 
 @Transactional
 public interface ProductService {
     
     PageResponseDTO<ProductListDTO> list(PageRequestDTO pageRequestDTO);
+
+    Long register(ProductDTO productDTO);
 }
