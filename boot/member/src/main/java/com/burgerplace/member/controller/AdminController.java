5ffe900810c7d5e840a3c005c79a923a7bc9c3f4
soapcandy <octi97@naver.com>
2023-08-13 22:28:42 +0900

    .

--- boot/member/src/main/java/com/burgerplace/member/controller/AdminController.java
+++ boot/member/src/main/java/com/burgerplace/member/controller/AdminController.java
@@ -1,30 +1,23 @@
 package com.burgerplace.member.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.burgerplace.member.dto.MemberDTO;
 import com.burgerplace.member.dto.PageRequestDTO;
 import com.burgerplace.member.dto.PageResponseDTO;
-import com.burgerplace.member.entity.Member;
-import com.burgerplace.member.repository.MemberRepository;
 import com.burgerplace.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
