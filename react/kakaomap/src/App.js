ca5fa312600d075d4731f48e08a19a7bc1b30863
soapcandy <octi97@naver.com>
2023-08-16 12:03:13 +0900

    kakaoMapTest

---
+++ react/kakaomap/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MapComponent from './component/MapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MapComponent></MapComponent>
+    </div>
+  );
+}
+
+export default App;
