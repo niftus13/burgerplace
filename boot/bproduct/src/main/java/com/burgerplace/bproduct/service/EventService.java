c7eeadd8b53d6a7bdb46bf8d2db3ff4410809f08
niftus13 <niftus13@gmail.com>
2023-08-16 11:19:04 +0900

    허정

---
+++ boot/bproduct/src/main/java/com/burgerplace/bproduct/service/EventService.java
@@ -0,0 +1,15 @@
+package com.burgerplace.bproduct.service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface EventService {
+
+    void parsing();
+
+    
+
+
+
+    
+}
